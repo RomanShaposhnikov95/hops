c451c02d6aa8d0e97c6ec5ceaa625bd95e6eb3ca
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2023-04-12 09:37:09 +0300

    add new indicator

---
+++ js/newTab.js
@@ -0,0 +1,23 @@
+const allCirc = document.querySelectorAll('.circ')
+const result = document.querySelector('.circ-result');
+
+
+allCirc.forEach(el => {
+    const controller = el.querySelector('input[type=range]');
+    const radialProgress = el.querySelector('.RadialProgress');
+
+    const setProgress = (progress) => {
+        const value = `${progress}%`;
+        radialProgress.style.setProperty('--progress', value)
+        if (el.classList.contains("circ-general")) {
+            result.innerHTML = value
+        }
+        radialProgress.setAttribute('aria-valuenow', value)
+    }
+
+    setProgress(controller.value)
+    controller.oninput = () => {
+        setProgress(controller.value)
+    }
+})
+
